5bf293adc0a63764715384a636e90ad190b004f9
Anwar Rahim <anwargithub@gmail.com>
2025-01-30 21:12:26 +0500

    initialize project structure with HTML, CSS, JS, and configuration files

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('text-activity').innerHTML = `<p>${data.activity}</p>`
+    })
