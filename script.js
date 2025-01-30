c2ebd87b8056428f1dacd3ab6a7752d6358e45d8
Anwar Rahim <anwargithub@gmail.com>
2025-01-30 21:35:28 +0500

    Update button functionality and add hover effect
    Renamed the button ID to match its purpose and added an event listener for fetching activities on click. Enhanced the button design by introducing a hover shadow effect for better user experience.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
 
-fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('text-activity').innerHTML = `<p>${data.activity}</p>`
-    })
+document.getElementById('activity-generator-btn').addEventListener('click',function (){
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('text-activity').innerHTML = `<p>${data.activity}</p>`
+        })
+})
+
+
